e3991286154c0381fcb970df203793d1afce7b6f
yuqiang zhang <yuqiangzhang.thu@gmail.com>
2012-11-29 14:23:23 -0500

    added purspose of program and handled divison by zero

--- mean.py
+++ mean.py
@@ -1,6 +1,13 @@
+# print the mean of the numbers given as arguments
+
 import sys
 
 sum = 0
+
+if len(sys, argv) == 1:
+    print "Error: no arguments given."
+    sys.exit()
+
 for num in sys.argv[1:]:
     sum +=float(num)
 
